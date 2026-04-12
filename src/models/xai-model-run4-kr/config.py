636fe5f6a71efbff3dd9cf43b05807e03ce7c7dc
rajshreeee <rajshreerai931@gmail.com>
2026-04-12 19:22:44 +0300

    run4: kr

---
+++ src/models/xai-model-run4-kr/config.py
@@ -0,0 +1,4 @@
+PROJECT_NAME = "mammography-kernel-week15-run3-ft"
+LOG_DIR = "/ediss_data/ediss2/xai-texture/src/models/xai-model-run4-kr/logs"
+CHECKPOINT_DIR = "/ediss_data/ediss2/xai-texture/src/models/xai-model-run4-kr/checkpoints"
+SPLIT_PATH = "/ediss_data/ediss2/xai-texture/src/models/xai-model-run4-kr/data/split_indices.json"
---
+++ src/models/xai-model-run5-ftkr/config.py
@@ -0,0 +1,5 @@
+PROJECT_NAME = "mammography-kernel-week15-run3-ft"
+LOG_DIR = "/ediss_data/ediss2/xai-texture/src/models/xai-model-run5-ftkr/logs"
+CHECKPOINT_DIR = "/ediss_data/ediss2/xai-texture/src/models/xai-model-run5-ftkr/checkpoints"
+SPLIT_PATH = "/ediss_data/ediss2/xai-texture/src/models/xai-model-run5-ftkr/data/split_indices.json"
+
